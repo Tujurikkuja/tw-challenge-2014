8aca14661d6d243df3ff26661473580161dbd3b8
Siim Somma <siim@greaton.ee>
2014-11-29 16:30:12 +0200

    Checkpoint rev++

--- demo/tracking.php
+++ demo/tracking.php
@@ -1,7 +1,53 @@
 <?php
 	session_start();
-	if($_POST){
-		$_SESSION = array_merge($_SESSION, $_POST);
-		var_dump($_POST);
-	}	
+	//include('config.php');
+	if(isset($_GET['id'])){
+	
+	}
 ?>
+<html>
+<head>
+</head>
+<body>
+<div id="wrapper">
+	<div id="leftConteiner">
+		<div id="info">
+			<span>Tracking id: 12345</span><br/>
+			<table>
+				<tr>
+					<td>Amount:</td>
+					<td>123.45 USD</td>
+				</tr>
+				<tr>
+					<td>From:</td>
+					<td>Tjonis Tjuul</td>
+				</tr>
+				<tr>
+					<td>To:</td>
+					<td>+372 56488652</td>
+				</tr>
+				<tr>
+					<td>Reference:</td>
+					<td>Lahe asi</td>
+				</tr>
+			<table>
+		</div>
+		<br/>
+		<div id="status">
+			<input type="checkbox" value="setup" disabled/> Order set-up.</br>
+			<input type="checkbox" value="moneyRec" disabled/> Money received.</br>
+			<input type="checkbox" value="recipNot" disabled/> Recipient notified.</br>
+			<input type="checkbox" value="recipAcc" disabled/> Recipient accepted.</br>
+			<input type="checkbox" value="currConv" disabled/> Currency converted.</br>
+			<input type="checkbox" value="paymSent" disabled/> Payment sent out.</br>
+		</div>
+	</div><br/>
+	<div id="rightContainer">
+	Lorem ipsum dolor sit amet, consequat nascetur, iaculis interdum, rutrum nec amet pellentesque velit, consequat non odio vel nulla posuere. In eget ante, venenatis quisque ac fermentum integer, gravida velit, a sit pede. Consequat facilisis quis ante. Lacus tortor lorem, non iaculis a tristique, dolor nulla mi platea luctus et mi, cursus luctus eget, pede erat ullamcorper ipsum. Sociis velit, nam malesuada odio, facilisis litora tempus lorem ac aliquam eget, ullamcorper urna ac. Vel in mi gravida, suspendisse curabitur aenean nonummy et, et et porta id euismod mauris, nam purus. Malesuada commodo mauris ipsum consectetuer eu tincidunt, amet hac ipsum lectus, elementum non nunc lorem tincidunt, rutrum morbi neque justo malesuada sed eleifend, ad eleifend justo nunc enim ligula maecenas. Repellendus ipsum lacus nunc, nam pharetra, rutrum vestibulum rutrum eros lacus tristique, arcu varius et, quis massa consequat id. Massa feugiat nullam leo sollicitudin ligula, integer placerat lorem arcu a, sit et praesent montes.
+Turpis potenti ultrices nunc wisi tellus, inceptos lorem, mauris tempor magnis porta tempor nec, fringilla non in vestibulum. Quia praesent massa, donec in dignissim nisl amet. Nonummy posuere temporibus sit blandit pretium justo, in mi dictumst nullam tellus felis massa. Malesuada mauris auctor dolor, eu habitant non nullam lacus dignissim. Varius eu justo, quis nisl magna cras, lobortis vivamus vitae, lorem mauris vestibulum imperdiet eleifend. Lacus at taciti, dolor vitae et scelerisque sem, ut aenean tellus risus. Risus volutpat sapien, diam sem tincidunt commodo mi neque, potenti maecenas leo, fames eget mi. Litora sociosqu montes, accumsan id. Odio quis, urna aliquam, a pede vestibulum faucibus.
+Massa nec velit, molestie molestie parturient velit quam eleifend nunc, sem a vehicula dictum pellentesque volutpat elit. Magna nec nisl lorem, eu feugiat fermentum, amet ullamcorper quam, facilisi maecenas turpis a quisque enim, id amet metus mauris rutrum. Libero mollis sit, aliquam suspendisse dui. Pharetra cras sit, dictum non consectetuer metus metus suspendisse, tristique libero et consequat sit purus. Dignissim nec consectetuer nullam, ad scelerisque magna rutrum at ligula justo, in nisl ac dolor elementum congue, malesuada porttitor, nec in pulvinar sint nunc ac blandit. Amet odio ut. Posuere placerat elit ultricies dignissim, erat sodales, consequat cras dictumst vivamus eu donec dolor, lectus ipsum et nulla. Porta bibendum. Per cursus nec urna platea maecenas, enim et blandit massa, blandit nunc maecenas sapien suscipit pede, dui egestas etiam metus, nam adipiscing sodales in. Integer et eget, fames pede sit. Sed a elit vestibulum erat lorem, pellentesque eros recusandae, vulputate gravida adipiscing eu tristique etiam.
+Suspendisse massa lectus diam tincidunt nibh odio, suspendisse eget fermentum nec nullam modi, est penatibus nam tempor vel, arcu nulla nam id. Cum duis, quis pellentesque consectetuer. Libero aliquam, maecenas mollis non ultricies nonummy felis. Porttitor tristique mauris tellus enim, mollis aliquam, blandit mollis, in mi nec nonummy eget, scelerisque velit hymenaeos et. Maecenas id, tortor ac, suscipit suscipit per adipisicing nec, metus magna lacus, a rutrum in. Feugiat magna etiam arcu magna magna enim. Purus hac, est nec lacinia eum pellentesque, elementum ut, ligula ut sed, libero ullamcorper lacus ad dolor quam. Mi ut vestibulum amet non tortor et. Enim pede, quisque a et elit tellus, pretium tellus metus fusce phasellus est ac, etiam ornare, quam amet sit vel. Mauris consectetuer elementum, cursus qui vel neque porttitor consequatur sem, dolor turpis velit donec enim posuere vitae, ut semper laoreet platea urna at. Condimentum turpis, adipiscing ac nibh eget magnam penatibus dolor, aliquam etiam lorem elit. Orci tristique ornare, urna duis, varius lectus wisi in vehicula.
+	</div>
+</div>
+</body>
+</html>
