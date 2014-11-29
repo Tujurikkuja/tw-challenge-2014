588ae1c6a951b4e47cfcb8c9d53aeaa6000c5231
tonis90 <tonis90@gmail.com>
2014-11-29 18:16:01 +0200

    Checkpoint 2 rev 9

--- demo/tracking.php
+++ demo/tracking.php
@@ -5,13 +5,58 @@
 	
 	}
 ?>
-<html>
-<head>
-</head>
-<body>
-<div id="wrapper">
-	<div id="leftConteiner">
-		<div id="info">
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>TransferWise BETA</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="starter-template.css" rel="stylesheet">
+	<link href="dashboard.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">TransferWise BETA</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="index.html">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+	
+	<div class="container">
+      <div class="row">
+        <div class="col-sm-3 col-md-3 sidebar">
+		<br/><br/>
 			<span>Tracking id: 12345</span><br/>
 			<table>
 				<tr>
@@ -20,7 +65,7 @@
 				</tr>
 				<tr>
 					<td>From:</td>
-					<td>Tjonis Tjuul</td>
+					<td>Tonis Tuul</td>
 				</tr>
 				<tr>
 					<td>To:</td>
@@ -30,24 +75,98 @@
 					<td>Reference:</td>
 					<td>Lahe asi</td>
 				</tr>
+			</table><br/>
+			<div id="status" class="row" style="margin-left:15px">
+				<input type="checkbox" value="setup" disabled/> Order set-up.</br>
+				<input type="checkbox" value="moneyRec" disabled/> Money received.</br>
+				<input type="checkbox" value="recipNot" disabled/> Recipient notified.</br>
+				<input type="checkbox" value="recipAcc" disabled/> Recipient accepted.</br>
+				<input type="checkbox" value="currConv" disabled/> Currency converted.</br>
+				<input type="checkbox" value="paymSent" disabled/> Payment sent out.</br>
+			</div>
+        </div>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+		<!-- 1 -->
+		<div id="Order set-up">		
+		<h1 class="page-header">Waiting to receive money</h1>
+		If you haven't already, ask your bank to send money to TransferWise<tr/>
 			<table>
-		</div>
-		<br/>
-		<div id="status">
-			<input type="checkbox" value="setup" disabled/> Order set-up.</br>
-			<input type="checkbox" value="moneyRec" disabled/> Money received.</br>
-			<input type="checkbox" value="recipNot" disabled/> Recipient notified.</br>
-			<input type="checkbox" value="recipAcc" disabled/> Recipient accepted.</br>
-			<input type="checkbox" value="currConv" disabled/> Currency converted.</br>
-			<input type="checkbox" value="paymSent" disabled/> Payment sent out.</br>
-		</div>
-	</div><br/>
-	<div id="rightContainer">
-	Lorem ipsum dolor sit amet, consequat nascetur, iaculis interdum, rutrum nec amet pellentesque velit, consequat non odio vel nulla posuere. In eget ante, venenatis quisque ac fermentum integer, gravida velit, a sit pede. Consequat facilisis quis ante. Lacus tortor lorem, non iaculis a tristique, dolor nulla mi platea luctus et mi, cursus luctus eget, pede erat ullamcorper ipsum. Sociis velit, nam malesuada odio, facilisis litora tempus lorem ac aliquam eget, ullamcorper urna ac. Vel in mi gravida, suspendisse curabitur aenean nonummy et, et et porta id euismod mauris, nam purus. Malesuada commodo mauris ipsum consectetuer eu tincidunt, amet hac ipsum lectus, elementum non nunc lorem tincidunt, rutrum morbi neque justo malesuada sed eleifend, ad eleifend justo nunc enim ligula maecenas. Repellendus ipsum lacus nunc, nam pharetra, rutrum vestibulum rutrum eros lacus tristique, arcu varius et, quis massa consequat id. Massa feugiat nullam leo sollicitudin ligula, integer placerat lorem arcu a, sit et praesent montes.
-Turpis potenti ultrices nunc wisi tellus, inceptos lorem, mauris tempor magnis porta tempor nec, fringilla non in vestibulum. Quia praesent massa, donec in dignissim nisl amet. Nonummy posuere temporibus sit blandit pretium justo, in mi dictumst nullam tellus felis massa. Malesuada mauris auctor dolor, eu habitant non nullam lacus dignissim. Varius eu justo, quis nisl magna cras, lobortis vivamus vitae, lorem mauris vestibulum imperdiet eleifend. Lacus at taciti, dolor vitae et scelerisque sem, ut aenean tellus risus. Risus volutpat sapien, diam sem tincidunt commodo mi neque, potenti maecenas leo, fames eget mi. Litora sociosqu montes, accumsan id. Odio quis, urna aliquam, a pede vestibulum faucibus.
-Massa nec velit, molestie molestie parturient velit quam eleifend nunc, sem a vehicula dictum pellentesque volutpat elit. Magna nec nisl lorem, eu feugiat fermentum, amet ullamcorper quam, facilisi maecenas turpis a quisque enim, id amet metus mauris rutrum. Libero mollis sit, aliquam suspendisse dui. Pharetra cras sit, dictum non consectetuer metus metus suspendisse, tristique libero et consequat sit purus. Dignissim nec consectetuer nullam, ad scelerisque magna rutrum at ligula justo, in nisl ac dolor elementum congue, malesuada porttitor, nec in pulvinar sint nunc ac blandit. Amet odio ut. Posuere placerat elit ultricies dignissim, erat sodales, consequat cras dictumst vivamus eu donec dolor, lectus ipsum et nulla. Porta bibendum. Per cursus nec urna platea maecenas, enim et blandit massa, blandit nunc maecenas sapien suscipit pede, dui egestas etiam metus, nam adipiscing sodales in. Integer et eget, fames pede sit. Sed a elit vestibulum erat lorem, pellentesque eros recusandae, vulputate gravida adipiscing eu tristique etiam.
-Suspendisse massa lectus diam tincidunt nibh odio, suspendisse eget fermentum nec nullam modi, est penatibus nam tempor vel, arcu nulla nam id. Cum duis, quis pellentesque consectetuer. Libero aliquam, maecenas mollis non ultricies nonummy felis. Porttitor tristique mauris tellus enim, mollis aliquam, blandit mollis, in mi nec nonummy eget, scelerisque velit hymenaeos et. Maecenas id, tortor ac, suscipit suscipit per adipisicing nec, metus magna lacus, a rutrum in. Feugiat magna etiam arcu magna magna enim. Purus hac, est nec lacinia eum pellentesque, elementum ut, ligula ut sed, libero ullamcorper lacus ad dolor quam. Mi ut vestibulum amet non tortor et. Enim pede, quisque a et elit tellus, pretium tellus metus fusce phasellus est ac, etiam ornare, quam amet sit vel. Mauris consectetuer elementum, cursus qui vel neque porttitor consequatur sem, dolor turpis velit donec enim posuere vitae, ut semper laoreet platea urna at. Condimentum turpis, adipiscing ac nibh eget magnam penatibus dolor, aliquam etiam lorem elit. Orci tristique ornare, urna duis, varius lectus wisi in vehicula.
-	</div>
-</div>
-</body>
+				<tr>
+					<td>Amount:</td>
+					<td>123.45 USD</td>
+				</tr>				
+				<tr>
+					<td>To:</td>
+					<td>TransferWise</td>
+				</tr>
+				<tr>
+					<td>Account number:</td>
+					<td>53640809</td>
+				</tr>
+				<tr>
+					<td>Bank name:</td>
+					<td>Barclays, United Kingdom</td>
+				</tr>
+				<tr>
+					<td>Reference:</td>
+					<td>Lahe asi</td>
+				</tr>
+			</table>
+        </div>
+		<!-- 2 -->
+		<div id="Money received">
+		<h1 class="page-header">Money received, trying to match transactions</h1>
+		This might take some time...Or is it already done? Refresh.<tr/>
+        </div>
+		<!-- 3 -->
+		<div id="Recipient notified">
+		<h1 class="page-header">Waiting for user to respond to sent SMS</h1>
+		If you haven't already, check your phone or ask the recipient to check theirs!<tr/>
+			<table>
+				<tr>
+					<td>Amount:</td>
+					<td>123.45 USD</td>
+				</tr>				
+				<tr>
+					<td>To:</td>
+					<td>+372 56488652</td>
+				</tr>
+				<tr>
+					<td>Reference:</td>
+					<td>Lahe asi</td>
+				</tr>
+			</table>
+        </div>
+		<!-- 4 -->
+		<div id="Recipient accepted">
+		<h1 class="page-header">Money transfered!</h1>
+		Great Success!<tr/>
+			<table>
+				<tr>
+					<td>Amount:</td>
+					<td>123.45 USD</td>
+				</tr>				
+				<tr>
+					<td>To:</td>
+					<td>+372 56488652</td>
+				</tr>
+				<tr>
+					<td>Reference:</td>
+					<td>Lahe asi</td>
+				</tr>
+			</table>
+        </div>
+		
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="import/jquery.min.js"></script>
+	<script src="import/jquery.form.js"></script> 
+    <script src="bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="ie10-viewport-bug-workaround.js"></script>
+  </body>
 </html>
